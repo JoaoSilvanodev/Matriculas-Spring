7b3f1acbba6897b9701d049c61bee15ea6f0cf83
JoaoSilvano <joao.silvano@ufms.br>
2026-08-28 00:16:28 -0400

    enable test profile for MatriculasSpringApplicationTests

--- MatriculasSpring/src/test/java/com/clogs/matriculaspring/service/AuthServiceTest.java
+++ MatriculasSpring/src/test/java/com/clogs/matriculaspring/service/AuthServiceTest.java
@@ -20,7 +20,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-@ActiveProfiles("test")
+
 public class AuthServiceTest {
 
     @Mock
